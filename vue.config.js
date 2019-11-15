2142270c057b318e8e0c4759ced768367b494fb6
dev <toto>
2019-11-15 11:45:49 +0100

    add: vuetify and vueI18n

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'transpileDependencies': [
+    'vuetify'
+  ],
+  pluginOptions: {
+    i18n: {
+      locale: 'en', // The locale of project localization
+      fallbackLocale: 'en', // The fallback locale of project localization
+      localeDir: 'locales', // The directory where store localization messages of project
+      enableInSFC: true // Enable locale messages in Single file components
+    }
+  }
+}
